d1d13332aef35e7b94da665b4b5379bea8c3caec
RigaZubaku <riga.zubaku@student.uni-pr.edu>
2024-12-18 03:29:04 +0100

    Added a video and seperated nav and footer files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
 const swiper = new Swiper(".swiper", {
     slidesPerView: 3,
     spaceBetween: 20,
