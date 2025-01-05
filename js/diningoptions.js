310423ad6488b7524d08ce6cd3595b4fd383edce
vaaleza <valezasutaj@gmail.com>
2025-01-05 03:34:39 +0100

    added html and css to read-more, datalist, output, multiple background , new js file for dining options...

---
+++ js/diningoptions.js
@@ -0,0 +1,86 @@
+const diningoptions = [
+    {
+        image: "../img/food1.webp",
+        category: "Casual Dining",
+        description: "Great flavor doesn’t need to come with lots of fuss. Enjoy grab-and-go goodness and amazing eats at casual, laid-back dining options — like the complimentary Windjammer, El Loco Fresh, Café Promenade, Dog House, and Sorrento’s Pizza."
+    },
+    {
+        image: "../img/food2.avif",
+        category: "Specialty Dining",
+        description: "Dig up dinner at upscale specialty cruise restaurants like Chops Grille℠ and Giovanni’s Italian Kitchen, go all out on an exclusive culinary journey at Chef’s Table, or savor global flavors one complimentary course at a time in the Main Dining Room."
+    },
+    {
+        image: "../img/food3.avif",
+        category: "Dine In",
+        description: "No matter what you’re in the mood for — and when — room service onboard our cruise ships delivers all your favorite cruise foods right to your stateroom, from juicy burgers, fresh salads and toasty sandwiches, to breakfast in bed with a couple of refreshing mimosas."
+    },
+    {
+        image: "../img/food4.webp",
+        category: "Bars & Lounges",
+        description: "If you’re craving cocktails, we offer amazing bars onboard our cruises where the vibe is always right and the drinks are always delicious. Like R Bar, known for its classic cocktails and retro vibes, or Boleros, where every sip comes paired with live salsa beats."
+    }
+];
+
+const diningswiper = [
+    {
+        image: "../img/foodswiper1.avif",
+        text: "MINI BITES"
+    },
+    {
+        image: "../img/foodswiper2.avif",
+        text: "WINDJAMMER"
+    },
+    {
+        image: "../img/foodswiper3.webp",
+        text: "EL LOCO FRESH"
+    },
+    {
+        image: "../img/foodswiper4.avif",
+        text: "DOG HOUSE"
+    },
+    {
+        image: "../img/foodswiper5.avif",
+        text: "SPLASHAWAY CAFÉ"
+    },
+    {
+        image: "../img/foodswiper6.avif",
+        text: "SOLARIUM BISTRO RESTAURANT"
+    },
+    {
+        image: "../img/foodswiper7.avif",
+        text: "CAFÉ PROMENADE"
+    },
+    {
+        image: "../img/foodswiper8.avif",
+        text: "NOODLE BAR"
+    },
+    {
+        image: "../img/foodswiper9.avif",
+        text: "SORRENTO'S PIZZA"
+    },
+    {
+        image: "../img/foodswiper10.avif",
+        text: "COASTAL KITCHEN"
+    },
+    {
+        image: "../img/foodswiper11.avif",
+        text: "THE CAFÉ @ TWO70"
+    },
+    {
+        image: "../img/foodswiper12.avif",
+        text: "PARK CAFÉ"
+    },
+    {
+        image: "../img/foodswiper13.avif",
+        text: "VITALITY CAFÉ"
+    },
+    {
+        image: "../img/foodswiper14.webp",
+        text: "MAIN DINING ROOM"
+    },
+    {
+        image: "../img/foodswiper15.avif",
+        text: "SURFSIDE EATERY"
+    }
+];
+
